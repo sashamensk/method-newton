77dc02cc2a36c6ae126d736c4b32f26eed080372
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-10-05 12:27:11 +0300

    change-settings

--- MethodNewtonTask/NumbersExtension.cs
+++ MethodNewtonTask/NumbersExtension.cs
@@ -9,7 +9,7 @@ namespace MethodNewtonTask
         /// <summary>
         /// Initializes static members of the <see cref="NumbersExtension"/> class.
         /// </summary>
-        static NumbersExtension() => AppSettings = new AppSettings { Epsilon = 0.1 };
+        static NumbersExtension() => AppSettings = new AppSettings { Epsilon = double.Epsilon };
 
         /// <summary>
         /// Find n-th root of number with the given accuracy.
@@ -29,7 +29,45 @@ namespace MethodNewtonTask
         /// </exception>
         public static double FindNthRoot(double number, int degree, double accuracy)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (double.IsInfinity(number) || double.IsNaN(number))
+            {
+                throw new ArgumentException($"{nameof(number)} is not a valid.");
+            }
+
+            if (accuracy < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(accuracy)} can not be less than zero.");
+            }
+
+            if (accuracy > NumbersExtension.AppSettings.Epsilon)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(accuracy)} should be less " +
+                                                      $"than {nameof(NumbersExtension.AppSettings.Epsilon)}");
+            }
+
+            if (degree < 1)
+            {
+                throw new ArgumentException($"{nameof(degree)} must be positive.");
+            }
+
+            if (number < 0.0 && degree % 2 == 0)
+            {
+                throw new ArgumentException($"It is impossible to calculate an ever {nameof(degree)}" +
+                                            $"if {nameof(number)} is negative.");
+            }
+
+            double previousApproximation = 1;
+            double currentApproximation = (1 / (double)degree) * (((double)(degree - 1) *
+                                                                   previousApproximation) + (number / Math.Pow(previousApproximation, degree - 1)));
+
+            while (Math.Abs(currentApproximation - previousApproximation) > accuracy)
+            {
+                previousApproximation = currentApproximation;
+                currentApproximation = (1 / (double)degree) * (((double)(degree - 1) *
+                                                                previousApproximation) + (number / Math.Pow(previousApproximation, degree - 1)));
+            }
+
+            return currentApproximation;
         }
     }
 }
