bfd75974f22a3625911536f120f38206541ebf43
sasha_mensk <aferovsky@gmail.com>
2021-03-11 18:59:07 +0300

    Done

--- MethodNewtonTask/NumbersExtension.cs
+++ MethodNewtonTask/NumbersExtension.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace MethodNewtonTask
 {
@@ -30,7 +30,38 @@ namespace MethodNewtonTask
         /// </exception>
         public static double FindNthRoot(double number, int degree, double accuracy)
         {
-            throw new NotImplementedException();
+            if (number < 0 && degree % 2 == 0)
+            {
+                throw new ArgumentException($"{nameof(number)}  is less than zero and degreee is even");
+            }
+
+            if (degree < 0)
+            {
+                throw new ArgumentException($"{nameof(degree)} is less than zero");
+            }
+
+            if (double.IsNaN(number) || double.IsInfinity(number))
+            {
+                throw new ArgumentException($"{nameof(number)} is NaN, double.NegativeInfinity, double.PositiveInfinity");
+            }
+
+            if (accuracy < 0 || accuracy > AppSettings.Epsilon)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(accuracy)}");
+            }
+
+            Random rand = new Random();
+            double temp = rand.Next(10);
+            double intMax = 2147483647;
+            double nnthRoot = 0.0;
+            while (intMax > accuracy)
+            {
+                nnthRoot = (((degree - 1.0) * temp) + ((double)number / Math.Pow(temp, degree - 1))) / (double)degree;
+                intMax = Math.Abs(nnthRoot - temp);
+                temp = nnthRoot;
+            }
+
+            return nnthRoot;
         }
-    }
+    }    
 }
